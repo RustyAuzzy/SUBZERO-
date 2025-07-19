168153941eea4f79a221af0e269d47777147ba5c
RustyAuzzy <154899786+RustyAuzzy@users.noreply.github.com>
2025-07-19 18:02:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;eQEUhSCL#QULaqi96dpJ_yz8ngI95ozB_ihj_g83GIs8t9IWmKVE", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2540100941348", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
